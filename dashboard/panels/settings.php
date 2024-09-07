9fec0e21b310f579e6af00266afa1120a39944be
Matthew Scott <11221111+Zei33@users.noreply.github.com>
2024-09-07 06:47:28 +0000

    More progress in scaffolding settings menu.

--- dashboard/panels/settings.php
+++ dashboard/panels/settings.php
@@ -3,12 +3,56 @@
 		<label class="inline-flex items-center mb-3 cursor-pointer">
 			<input type="checkbox" value="" class="sr-only peer" name="use-custom-base-url" id="use-custom-base-url">
 			<div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:w-5 after:h-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-			<span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Use Custom Base URL</span>
+			<span class="ms-3 text-sm font-medium">Use Custom Base URL</span>
 		</label>
 		<div class="mt-1 mb-2 hidden" id="custom-base-url-container">
-			<label for="custom-base-url" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Custom Base URL</label>
+			<label for="custom-base-url" class="block mb-2 text-sm font-medium">Custom Base URL</label>
 			<input type="text" id="custom-base-url" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="https://example.com" required />
 		</div>
 		<p class="mb-2 text-xs">Set a custom base URL only if your dashboard uses a different domain name to the primary website. When disabled, recordings will use the page URL to determine the base URL.</p>
 	</form>
+	<div class="w-full">
+		<form class="w-full mb-4">
+			<div class="relative mb-8">
+				<label for="heatmap-resolution-range">Heatmap Resolution</label>
+				<input id="heatmap-resolution-range" type="range" value="1" min="1" max="168" class="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700">
+				<span class="text-sm text-gray-500 dark:text-gray-400 absolute start-0 -bottom-6">1 hour</span>
+				<span id="current-heatmap-resolution-range" class="text-sm text-green-500 dark:text-green-400 absolute start-1/2 -translate-x-1/2 rtl:translate-x-1/2 -bottom-6">1 hour</span>
+				<span class="text-sm text-gray-500 dark:text-gray-400 absolute end-0 -bottom-6">7 days</span>
+			</div>
+			<p class="mb-2 text-xs">Heatmap data will be collated into periods based on the resolution. A lower resolution will let you observe data down to that level (e.g. 1 hour will let you see heatmaps from hour to hour). But lower resolutions require more database capacity and resources.</p>
+		</form>
+		<form class="w-full mb-4">
+			<label for="heatmap-breakpoints-select" class="block mb-2 text-sm font-medium">Heatmap Display Breakpoints</label>
+			<select id="heatmap-breakpoints-select" class="mb-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+				<option value="bootstrap" selected>Bootstrap</option>
+				<option value="tailwind">Tailwind</option>
+				<option value="foundation">Foundation</option>
+				<option value="custom">Custom</option>
+			</select>
+			<div class="flex justify-center">
+				<button type="button" id="show-heatmap-breakpoints-list" class="w-full py-2.5 px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Show breakpoints</button>
+			</div>
+			<div id="heatmap-breakpoints-list-container" class="relative overflow-x-auto shadow-md sm:rounded-lg hidden">
+				<table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+					<thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+						<tr>
+							<th scope="col" class="px-6 py-3">
+								Label
+							</th>
+							<th scope="col" class="px-6 py-3">
+								Starting Pixels >=
+							</th>
+							<th scope="col" class="px-6 py-3">
+								<span class="sr-only">Delete</span>
+							</th>
+						</tr>
+					</thead>
+					<tbody id="heatmap-breakpoints-list">
+					</tbody>
+				</table>
+			</div>
+			<p class="mt-2 text-xs">Users have many different display sizes and responsive websites will adjust their layout to support them. If you use a popular responsive framework, select it from the list. Otherwise you can setup your own. Then your heatmap data will be organised by display size.</p>
+		</form>
+	</div>
 </div>
--- scripts/dashboard/EasySettings.js
+++ scripts/dashboard/EasySettings.js
@@ -8,6 +8,8 @@ class EasySettings {
 	init() {
 		this.settingsContainer = this.dashboard.contentContainer.querySelector('#easy-settings-container');
 		this.bindButtons();
+		this.bindHeatmapResolutionRange();
+		this.bindHeatmapBreakpointSelect();
 	}
 
 	bindButtons() {
@@ -20,5 +22,102 @@ class EasySettings {
 				customBaseUrlContainer.classList.add('hidden');
 			}
 		});
+	}
+
+	bindHeatmapResolutionRange() {
+		const rangeElement = this.settingsContainer.querySelector('#heatmap-resolution-range');
+		const currentValueElement = this.settingsContainer.querySelector('#current-heatmap-resolution-range');
+		const snapValues = [1, 3, 6, 12, 24, 48, 72, 96, 120, 144, 168];
+
+		rangeElement.addEventListener('input', function() {
+			const closestValue = snapValues.reduce((prev, curr) => {
+				return (Math.abs(curr - rangeElement.value) < Math.abs(prev - rangeElement.value) ? curr : prev);
+			});
+			rangeElement.value = closestValue;
+	
+			if (closestValue >= 24) {
+				const days = closestValue / 24;
+				currentValueElement.textContent = `${days} ${days === 1 ? 'day' : 'days'}`;
+			} else {
+				currentValueElement.textContent = `${closestValue} ${closestValue === 1 ? 'hour' : 'hours'}`;
+			}
+		});
+	}
+
+	bindHeatmapBreakpointSelect() {
+		const selectElement = this.settingsContainer.querySelector('#heatmap-breakpoints-select');
+		this.generateBreakpointList(selectElement.value);
+		
+		selectElement.addEventListener('change', () => {
+			this.generateBreakpointList(selectElement.value);
+		});
+
+		const listContainer = this.settingsContainer.querySelector('#heatmap-breakpoints-list-container');
+		const showListButton = this.settingsContainer.querySelector('#show-heatmap-breakpoints-list');
+		showListButton.addEventListener('click', () => {
+			listContainer.classList.toggle('hidden');
+			showListButton.parentNode.classList.toggle('hidden');
+		});
+	}
+
+	generateBreakpointList(preset) {
+		let breakpoints = [];
+		switch (preset) {
+			case "bootstrap":
+				breakpoints = [
+					{ name: "Extra Small", value: 0, disabled: true },
+					{ name: "Small", value: 576, disabled: true },
+					{ name: "Medium", value: 768, disabled: true },
+					{ name: "Large", value: 992, disabled: true },
+					{ name: "Extra Large", value: 1200, disabled: true },
+					{ name: "XX Large", value: 1400, disabled: true }
+				];
+				break;
+			case "tailwind":
+				breakpoints = [
+					{ name: "Extra Small", value: 0, disabled: true },
+					{ name: "Small", value: 640, disabled: true },
+					{ name: "Medium", value: 768, disabled: true },
+					{ name: "Large", value: 1024, disabled: true },
+					{ name: "Extra Large", value: 1280, disabled: true },
+					{ name: "XX Large", value: 1536, disabled: true }
+				];
+				break;
+			case "foundation":
+				breakpoints = [
+					{ name: "Small", value: 0, disabled: true },
+					{ name: "Medium", value: 641, disabled: true },
+					{ name: "Large", value: 1025, disabled: true },
+					{ name: "Extra Large", value: 1201, disabled: true },
+					{ name: "XX Large", value: 1441, disabled: true }
+				];
+				break;
+			default:
+				breakpoints = [
+					{ name: "Extra Small", value: 0, disabled: true }
+				];
+		}
+
+		const listContainer = this.settingsContainer.querySelector('#heatmap-breakpoints-list');
+		listContainer.innerHTML = '';
+		for (const breakpoint of breakpoints) {
+			listContainer.innerHTML += `
+				<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+					<th scope="row" class="px-2 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+						<input type="text" class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value="${breakpoint.name}" ${breakpoint.disabled ? "disabled": ""}>
+					</th>
+					<td class="px-2 py-2">
+						<input type="number" class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value="${breakpoint.value}" ${breakpoint.disabled ? "disabled": ""}>
+					</td>
+					<td class="px-6 py-4 text-right">
+						${
+							breakpoint.disabled ? 
+							`<span class="font-medium text-gray-600 dark:text-gray-500">Delete</span>` : 
+							`<a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Delete</a>`
+						}
+					</td>
+				</tr>
+			`;
+		}
 	}
 }
